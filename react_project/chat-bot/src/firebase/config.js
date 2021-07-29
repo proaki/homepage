ac506589ee664671389f2c05824460ed98637a3f
Hiroaki-Suzuki <suzuhiro5145@gmail.com>
2021-07-29 12:41:23 +0900

    chat-bot_demo

---
+++ react_project/chat-bot/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD29FSpu6v4_kNvozEl95qRO0vKpxD9Lx0",
+  authDomain: "chatbot-demo-eff43.firebaseapp.com",
+  projectId: "chatbot-demo-eff43",
+  storageBucket: "chatbot-demo-eff43.appspot.com",
+  messagingSenderId: "1048343629353",
+  appId: "1:1048343629353:web:0dcaee11b72e8bb2665638",
+  measurementId: "G-2FPTS3L2XQ"
+};
+
+export default firebaseConfig
