ea88e8c3b9cc6503e834fbdb6b7a4ec5cbd408e8
Hiroaki-Suzuki <suzuhiro5145@gmail.com>
2021-10-22 15:43:32 +0900

    add_picture

---
+++ react_project/homepage/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_irCIPwEGrCVlJLjL4v1XkCz-6TKfV6I",
+//   authDomain: "homepage-8f82e.firebaseapp.com",
+//   projectId: "homepage-8f82e",
+//   storageBucket: "homepage-8f82e.appspot.com",
+//   messagingSenderId: "237808603459",
+//   appId: "1:237808603459:web:7bcdc4e0decb9a53279ff6",
+//   measurementId: "G-HQGHN92EJW"
+// };
+
+// export default firebaseConfig
